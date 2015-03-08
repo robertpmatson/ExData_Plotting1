d7a234cc0ba56781fc210804795e6ae6b741c56a
Robert Paul Matson <robert@neomailbox.ch>
2015-03-08 23:29:09 +0100

    Last code update

--- plot4.R
+++ plot4.R
@@ -15,10 +15,16 @@ plot4 <- function(){
   
   # Create the plot
   # Note: Using with( data, { ... }) did not work without add data frame name so did not use
+  
+  # Topleft
   plot(data$dateTime, data$Global_active_power, pch=21, col="white",xlab="",ylab="Global Active Power")
   lines(data$dateTime, data$Global_active_power)
+  
+  #Top right
   plot(data$dateTime, data$Voltage, pch=21,col="white" , xlab="datetime", ylab="Voltage")
   lines(data$dateTime, data$Voltage)
+  
+  #Bottom left
   plot(data$dateTime, data$Sub_metering_1, pch=21, col="white",xlab="",ylab="Energy sub metering")
   points(data$dateTime, data$Sub_metering_2, pch=21,col="white" )
   points(data$dateTime, data$Sub_metering_3,pch=21, col="white" )
@@ -26,6 +32,8 @@ plot4 <- function(){
   lines(data$dateTime, data$Sub_metering_2, col="red")
   lines(data$dateTime, data$Sub_metering_3, col="blue")
   legend("topright", col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=1, lty=c(1,1,1), box.col = "white", inset=0.01)
+  
+  #Bottom right 
   plot(data$dateTime, data$Global_reactive_power, pch=21,col="white" , xlab="datetime", ylab="Global_reactive_power")
   lines(data$dateTime, data$Global_reactive_power)
   
