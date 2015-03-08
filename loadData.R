e9ce64bb03292f1324be8d5bc7de6a61422823ff
Robert Paul Matson <robert@neomailbox.ch>
2015-03-08 19:14:27 +0100

    Added more comments

--- loadData.R
+++ loadData.R
@@ -1,6 +1,8 @@
 loadData <- function(){
   
+  # LIbrary used for date processing
   library(lubridate)
+  # LIbrary used for data frame processing
   library(dplyr)
 
   # Import all data as character. We cannot import numerics directly as numeric
@@ -13,9 +15,12 @@ loadData <- function(){
   # LOad as a table
   data <- tbl_df(data)
   
+  # Filter for the dates
   data <- filter(data, dmy(Date) >= ymd("2007/02/01") & dmy(Date) <= ymd("2007/02/02"))
+  # Remove entries without a numeric
   data <- filter(data, Global_active_power != "?")
   
+  # Set numeric columns to numeric types
   data <- mutate(data, Global_active_power = as.numeric(Global_active_power))
   data <- mutate(data, Global_reactive_power = as.numeric(Global_reactive_power))
   data <- mutate(data, Voltage = as.numeric(Voltage))
@@ -23,6 +28,7 @@ loadData <- function(){
   data <- mutate(data, Sub_metering_1 = as.numeric(Sub_metering_1))
   data <- mutate(data, Sub_metering_2 = as.numeric(Sub_metering_2))
   data <- mutate(data, Sub_metering_3 = as.numeric(Sub_metering_3))
+  # Add a column that has the Date and Time columns appended and converted to a posixCT date time
   data <- mutate(data, dateTimeDate = dmy_hms(paste(Date, Time)))
   data
   
